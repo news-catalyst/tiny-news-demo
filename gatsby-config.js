d318b092733c1f359f40a6b6c31fd6eb32b952d1
Jacqui Lough <lough.jacqui@gmail.com>
2020-05-04 08:04:23 +1000

    Testing integration of markdown pages with source and transformer plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,5 +7,15 @@
 module.exports = {
   /* Your site config here */
   plugins: 
-    [`gatsby-plugin-netlify-cms`],
+    [
+      `gatsby-plugin-netlify-cms`,
+      {
+        resolve: `gatsby-source-filesystem`,
+        options: {
+          name: `markdown-pages`,
+          path: `${__dirname}/src/markdown-pages`,
+        },
+      },
+      `gatsby-transformer-remark`,
+    ]
 }
