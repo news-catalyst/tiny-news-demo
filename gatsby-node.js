0e3a0a57a28ec2ab16dcc13e54491c6e3c40e5f4
Jacqui Lough <lough.jacqui@gmail.com>
2020-05-05 09:40:49 +1000

    Repointed to top-level google docs folder and updated query to exclude drafts when making pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -6,7 +6,7 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
   graphql(
     `
         {
-            allGoogleDocs {
+            allGoogleDocs(filter: {document: {breadcrumb: {nin: "Drafts"}}}) {
                 nodes {
                     document {
                         path
@@ -25,36 +25,4 @@ exports.createPages = async ({ actions, graphql, reporter }) => {
       })
   })
 
-  // const blogPostTemplate = path.resolve(`src/templates/blogTemplate.js`)
-
-  // const result = await graphql(`
-  //   {
-  //     allMarkdownRemark(
-  //       sort: { order: DESC, fields: [frontmatter___date] }
-  //       limit: 1000
-  //     ) {
-  //       edges {
-  //         node {
-  //           frontmatter {
-  //             path
-  //           }
-  //         }
-  //       }
-  //     }
-  //   }
-  // `)
-
-  // // Handle errors
-  // if (result.errors) {
-  //   reporter.panicOnBuild(`Error while running GraphQL query.`)
-  //   return
-  // }
-
-  // result.data.allMarkdownRemark.edges.forEach(({ node }) => {
-  //   createPage({
-  //     path: node.frontmatter.path,
-  //     component: blogPostTemplate,
-  //     context: {}, // additional data can be passed via context
-  //   })
-  // })
 }
