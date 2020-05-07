29077f58a0122585950ed468f6006d5057eedb29
Jacqui Lough <lough.jacqui@gmail.com>
2020-05-08 08:55:14 +1000

    Added a navbar to article template

--- src/templates/post.js
+++ src/templates/post.js
@@ -14,6 +14,42 @@ export default function Post({ data }) {
 
   return (
     <div>
+                <nav className="navbar" role="navigation" aria-label="main navigation">
+                  <div className="navbar-brand">
+                    <a className="navbar-item" href="/">
+                      tinynewsco
+                    </a>
+
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                      <span aria-hidden="true"></span>
+                      <span aria-hidden="true"></span>
+                      <span aria-hidden="true"></span>
+                    </a>
+                  </div>
+
+                  <div className="navbar-menu">
+                    <div className="navbar-start">
+                      <a className="navbar-item" href="/">
+                        Articles
+                      </a>
+
+                      <a className="navbar-item" href="/topics">
+                        Topics
+                      </a>
+                    </div>
+
+                    <div className="navbar-end">
+                      <div className="navbar-item">
+                        <div className="buttons">
+                            <a className="button logout" href="/tinycms">
+                              tinycms
+                            </a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </nav>
+
       <Layout>
         <h1 className="title is-1">{doc.name}</h1>
         <h3 className="subtitle">By {doc.author}</h3>
