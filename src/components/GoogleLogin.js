62197ae7af7bcd34f12a0dc55434fb9a12eb4940
Jacqui Lough <lough.jacqui@gmail.com>
2020-05-08 06:55:21 +1000

    Added navbar to logged out state of tinycms editor

--- src/components/GoogleLogin.js
+++ src/components/GoogleLogin.js
@@ -285,11 +285,54 @@ class GoogleLogin extends Component {
           );
         } else {
             return (
+              <div>
+
+                <nav className="navbar" role="navigation" aria-label="main navigation">
+                  <div className="navbar-brand">
+                    <a className="navbar-item" href="/tinycms">
+                      tinycms
+                    </a>
+
+                    <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+                      <span aria-hidden="true"></span>
+                      <span aria-hidden="true"></span>
+                      <span aria-hidden="true"></span>
+                    </a>
+                  </div>
+
+                  <div className="navbar-menu">
+                    <div className="navbar-start">
+                      <a className="navbar-item" href="/tinycms">
+                        Articles
+                      </a>
+
+                    </div>
+
+                    <div className="navbar-end">
+                      <div className="navbar-item">
+                        <div className="buttons">
+                          {this.state.user && 
+                            <a id="" className="button logout" onClick={this.signOut}>
+                              Log out
+                            </a>
+                          }
+                          {!this.state.user && 
+                            <a id="customBtn" className="button is-light">
+                              Log in
+                            </a>
+                          }   
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </nav>
+
               <Layout>
                 <h1 className="title is-1">tinycms metadata editor</h1>
                 <h3 className="subtitle">Please login and authorize Google Drive access to continue.</h3>
 
               </Layout>
+              </div>
             );
         }
     }
