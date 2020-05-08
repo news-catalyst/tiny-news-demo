00fb3dc368b8794cddd27b0960e92e5aa4502cbc
Jacqui Lough <lough.jacqui@gmail.com>
2020-05-08 14:50:51 +1000

    Default image max width to 650px

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,7 +25,8 @@ module.exports = {
                 // It's important to specify the maxWidth (in pixels) of
                 // the content container as this plugin uses this as the
                 // base for generating different widths of each image.
-                maxWidth: 590,
+                maxWidth: 650,
+                wrapperStyle: "margin-top: 10px; margin-bottom: 10px;"
               },
             },
           ],
