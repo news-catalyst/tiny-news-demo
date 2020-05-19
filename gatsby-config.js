72ee6b00e341c30ce0208845c1e7f6255665667c
Jacqui Lough <lough.jacqui@gmail.com>
2020-05-19 14:29:30 +1000

    Trying to speed up the publishing process, with extra netlify config directives and caching plugin for netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,6 +82,7 @@ module.exports = {
       },
       `gatsby-plugin-sharp`,
       `gatsby-plugin-sass`,
+      `gatsby-plugin-netlify-cache`,
       {
         resolve: `gatsby-plugin-amp`,
         options: {
