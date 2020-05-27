1b8da9e8dc9ce4afddebb0edc3b4a1cb70ed2dff
Jacqui Lough <lough.jacqui@gmail.com>
2020-05-27 15:46:43 +1000

    Added typography using Noriega theme to this demo

--- gatsby-config.js
+++ gatsby-config.js
@@ -100,7 +100,12 @@ module.exports = {
           },
         },
       },
-
+      {
+        resolve: `gatsby-plugin-typography`,
+        options: {
+          pathToConfigModule: `src/utils/typography`,
+        },
+      },
       {
         resolve: `gatsby-plugin-amp`,
         options: {
---
+++ src/utils/typography.js
@@ -0,0 +1,9 @@
+import Typography from "typography"
+import noriegaTheme from 'typography-theme-noriega'
+
+
+const typography = new Typography({
+  noriegaTheme
+})
+
+export default typography
