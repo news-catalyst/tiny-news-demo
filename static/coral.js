9c47d81b5e72f820e23d2518ac13e292df54998b
Jacqui Lough <lough.jacqui@gmail.com>
2020-06-22 14:04:41 +1000

    Pointing Coral at heroku installation

--- static/coral.js
+++ static/coral.js
@@ -1,21 +1,23 @@
 (function() {
     let d = document, s = d.createElement('script');
-    var url = 'localhost:3000';
-    s.src = '//' + url + '/assets/js/embed.js';
+    let storyURL = window.location.href;
+    // s.src = '//' + url + '/assets/js/embed.js';
+    s.src = 'https://coral-talk-news-catalyst.herokuapp.com/assets/js/embed.js';
     s.async = false;
     s.defer = true;
     s.onload = function() {
         Coral.createStreamEmbed({
             id: "coral_thread",
             autoRender: true,
-            rootURL: '//' + url,
+            // rootURL: '//' + url,
+            rootURL: 'https://coral-talk-news-catalyst.herokuapp.com',
             // Uncomment these lines and replace with the ID of the
             // story's ID and URL from your CMS to provide the
             // tightest integration. Refer to our documentation at
             // https://docs.coralproject.net for all the configuration
             // options.
             // storyID: '${storyID}',
-            // storyURL: '${storyURL}',
+            storyURL: storyURL,
         });
     };
     (d.head || d.body).appendChild(s);
